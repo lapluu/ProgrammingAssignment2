3dbd5ca7522e65471b18d389013fdeb7ec11d6aa
lapluu <lapluu88@gmail.com>
2015-11-21 10:24:35 -0500

    Update cachematrix.R
    Add better documentation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,13 @@
 ## makeCacheMatrix and cacheSolve are pair of functions that
 ## caches the inverse of a matrix. 
-## first, you have to get an instance of the makeCacheMatrix
-## as makecm <- makeCacheMatrix(). Then You can create a square
-## matrix by yourself and uses the makecm$setMatrix() to store
-## the matrix into the cache. Optionally makecm$createMatrix()
-## provides a convenience function to create a square method by
-## supplying the desired dimension as argument. Then, you
+## first, call makeCacheMatrix to create a default square
+## matrix or you can specify the dimension of the square matrix.
+## Then, you
 ## invoke the cacheSolve(makecm) to compute the inverse of the
 ## matrix. 
 ##    If you call the cacheSolve(makecm) second time, it will
 ## return the inverse matrix in the cache. 
-##    If you store a new matrix into the makecm, by calling
-## makecm$setMatrix(), the matrix version counter will increment
-## by one to indicate that the matrix has been changed. 
-## If you call the cacheSolve(makecm) again, cacheSolve(makecm)
-## detect that the version of the inverse matrix is NOT matched
-## to the version of matrix;therefore, it will compute a new
-## inverse matrix and returns this new inverse version instead.
-
-## makeCacheMatrix function creates a special "matrix" object that 
-## can cache its inverse. Both the matrix and its inverse
-## have version counter to keep track of versioning.
+##    You can also assign an invertable to the makeCacheMatrix
 
 makeCacheMatrix <- function( m = NULL, dimension = 2) {
   ## This function make a sqaure matrix with
@@ -45,17 +32,6 @@ makeCacheMatrix <- function( m = NULL, dimension = 2) {
   }
   ## Return the matrix m
   get <- function()  m
-  ## optional function to create a square matrix
-  ## with random values for ease of testing purpose
-  ## The original assigment requirement does not
-  ## have this requirement
-  createMatrix <- function(dimension =2){
-    if(dimension == 0){
-      return(m)
-    }
-    set.seed(99)
-    m <- matrix(rnorm(dimension^2), dimension, dimension)
-  }
   setInverse <- function(inv){
     invm <<- inv
   } 
@@ -65,7 +41,6 @@ makeCacheMatrix <- function( m = NULL, dimension = 2) {
   ## return a square matrix with random numbers
   list(set = set, 
        get = get,
-       createMatrix = createMatrix,
        setInverse = setInverse,
        getInverse = getInverse)
 }
